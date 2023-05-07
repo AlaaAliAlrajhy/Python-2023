b5b083691cc0cce696118c984326ff6ac55f3d83
AlaaAliAlrajhy <131582181+AlaaAliAlrajhy@users.noreply.github.com>
2023-05-07 21:59:03 +0200

    Added Strings and String Operations 07-05.py file

---
+++ lesson_4.py
@@ -0,0 +1,33 @@
+def greet(name):
+    print("Hello "+ name)
+
+greet("Alaa")
+greet("Nour")
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+result = sum_numbers(3, 4)
+print(result)
+#
+def power(base, exp):
+
+    output = exp ** base
+    return output
+     
+print(power(4,2))
+#
+def even_or_odd():
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+number = int(input("Enter a number)):
+print("the number", number, "is", even_or_odd)
+
+import match
+import random
+
+print(math.cei)
+
+# %%
+
